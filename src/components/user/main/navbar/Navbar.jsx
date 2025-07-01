d9583ed997b5614ff48ccdd0739a8ade4ece0d99
vishal-rangdhol <vishalrangdhol@gmail.com>
2025-07-01 11:58:49 +0530

    done with navbar with fuctionalities

--- src/components/user/main/navbar/Navbar.jsx
+++ src/components/user/main/navbar/Navbar.jsx
@@ -1,18 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom';
 
 import { CiMenuKebab } from "react-icons/ci";
+import { FaRegWindowClose } from "react-icons/fa";
+import { RxCross1 } from "react-icons/rx";
+
 
 const Navbar = () => {
+
+  let [show, SetShow] = useState(false)
+
+  let handleShow = () => {
+    SetShow(!show)
+  }
+  console.log(show);
+
   return (
-    <div className = 'w-full h-full  '>
-      <nav className = 'bg-blue-500 h-[70px] w-full flex text-2xl justify-around items-center'>
-        <div><CiMenuKebab /></div>
-        <div>Home</div>
-        <div>Add bills</div>
-        <div>Filter bills</div>
-        <div>About</div>
+
+      <nav className = {`w-full h-[80px] bg-gray-500 flex items-center justify-around text-2xl sm-max:px-8
+          ${show? 'h-[210px]  flex flex-col justify-around gap-2.5 items-start p-2.5' : ""} 
+          sm:flex-row sm:h-[80px] sm:justify-around sm:items-center`}>
+        
+        <div className = 'hidden max-sm:flex '>
+          {
+            show? <RxCross1 onClick = {handleShow} className = ''/> : <CiMenuKebab onClick = {handleShow}/> 
+          }
+        </div>
+
+        <div className = {`max-sm:${show? 'block' : "hidden"}`}>
+          <NavLink to = '/home' className = {({isActive}) => isActive? "bg-amber-300 rounded-lg px-3 mg " : ""}>Home</NavLink>
+        </div>
+
+        <div className = {`max-sm:${show? 'block' : "hidden"}`}>
+          <NavLink to = 'addBills' className = {({isActive}) => isActive? "bg-amber-300 rounded-lg px-2" : ""}>Add Bills</NavLink>
+        </div>
+
+        <div className = {`max-sm:${show? 'block' : "hidden"}`}>
+          <NavLink to = 'filterBills' className = {({isActive}) => isActive? "bg-amber-300 rounded-lg px-2" : ""}>Filter Bills</NavLink>
+        </div>
+
+        <div className = {`max-sm:${show? 'block' : "hidden"}`}>
+          <NavLink to = 'about' className = {({isActive}) => isActive? "bg-amber-300 rounded-lg px-2" : ""}>About</NavLink>
+        </div>
+
       </nav>
-    </div>
+    // </div>
   )
 }
 
